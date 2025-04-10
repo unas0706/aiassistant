288a821fc998909d1498ef3ca1f89361bf9639f7
unas0706 <sk.unas 123@gmail.com>
2025-04-10 23:08:04 +0530

    updated login btn

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,14 @@
-import React from 'react';
-
-const Navbar = () => {  
-
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        
         <h1 className="navbar-title">Tech Store</h1>
       </div>
+      <Link to="/login" id="login-btn">
+        login
+      </Link>
     </nav>
   );
 };
